41ab1e768fb9711ba4857832cc0dcaebbe8ef3bc
MantehApps <mantehapps@gmail.com>
2021-04-11 01:40:18 +0200

    Initial Commit

---
+++ PlatinumCars/PlatinumCarsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlatinumCarsApp.swift
+//  PlatinumCars
+//
+//  Created by Mantas Simanauskas on 2021-04-11.
+//
+
+import SwiftUI
+
+@main
+struct PlatinumCarsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
